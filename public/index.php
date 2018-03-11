6d6014bc630f3479f7e0da7cb1fed496bdd212bd
Alexander Shatalov <alexander.shataloff@gmail.com>
2018-03-11 19:18:02 +0300

    minor revisions: form, cyrillic, upload file size, mimetype

--- public/index.php
+++ public/index.php
@@ -2,13 +2,15 @@
 
 /*
  * To-Do:
- * - Форма для загрузки файлов
- * - Распаковка файлов во временную папку
- * - Исправление файла Document.xml
- * - Создание архива с новым файлом, добавление к оригиналу fix
- * - Выдача клиенту
+ * + Ошибки при работе с файлами на кириллице
+ * + Форма для загрузки файлов
+ * + Распаковка файлов во временную папку
+ * + Исправление файла Document.xml
+ * + Создание архива с новым файлом, добавление к оригиналу fix
+ * + Выдача клиенту
+ * - Рефакторинг
  *
- * - Библиотека для работы с ZIP
+ * + Библиотека для работы с ZIP
  * - Config
  * - MVC (Yii2)
  * - Безопасность temp (права на папки, соответствие типу)
@@ -21,6 +23,7 @@ const DIR_TMP = "./temp/";
 class Fix
 {
     public $fileName;
+    public $originalFileName;
     public $tmpFileName;
     public $newFileName;
     public $rndDirName;
@@ -31,9 +34,15 @@ class Fix
 
     public function __construct()
     {
-        $this->fileName = $_FILES['file']['name'];
+        $this->fileName = $this->timeStampFileName();
+        $this->originalFileName = $_FILES['file']['name'];
         $this->tmpFileName = $_FILES['file']['tmp_name'];
         $this->run();
+    }
+
+    public function timeStampFileName()
+    {
+        return date("Y-m-d_H-i-s") . "_fix.mmap";
     }
 
     public function readFile()
@@ -60,7 +69,7 @@ class Fix
     public function rndDirCreate($fileName)
     {
         $this->rndDirName = DIR_TMP . md5($fileName . time()) . "/";
-        mkdir($this->rndDirName, 644);
+        mkdir($this->rndDirName, 0644);
     }
 
     public function unZip()
@@ -94,8 +103,9 @@ class Fix
 
     public function viewUrl() {
         echo '<br>Исправление файла прошло успешно!
+        <hr>Загруженный файл ' . $this->originalFileName . ' был переименован в ' . $this->fileName . '
         <br>Ссылка для скачивания (доступно 24 часа):
-        <a href="' . $this->newFileName . '">' . $this->newFileName . '</a>
+        <a href="' . $this->newFileName . '">' . $this->fileName . '</a>
         <hr>
         <a href="../../">Загрузить ещё 1 файл!</a> 
         ';
@@ -122,21 +132,26 @@ class Fix
         // Заменям старый файл на исправленный
         $this->replaceZip();
 
-        // Отображаем ссылку для скачивания
+        // Отображаем текст и ссылку для скачивания
         $this->viewUrl();
+
+        // Изменяем режим доступа к файлам
+        chmod($this->newFileName, 0444);
     }
 
 }
 
-if ($_SERVER['REQUEST_METHOD'] == "POST" && $_FILES['file']['type'] == "application/vnd.mindjet.mindmanager") {
+if ($_SERVER['REQUEST_METHOD'] == "POST"
+    && $_FILES['file']['type'] == "application/vnd.mindjet.mindmanager"
+    && $_FILES['file']['size'] < 10485760) {
 //    var_dump($_SERVER['REQUEST_METHOD']);
 //    var_dump($_FILES);
     $app = new Fix();
 } else {
     echo '
-        <b>Загрузка MindMap файла (*.mmap):</b>
+        <b>Загрузка MindMap файла (*.mmap) < 10Мб:</b>
             <form action="" enctype="multipart/form-data" method="post">
-                <input type="file" name="file"/>
+                <input type="file" name="file" accept="application/vnd.mindjet.mindmanager"/>
                 <input type="submit" value="Загрузить"/>
             </form>
     ';
